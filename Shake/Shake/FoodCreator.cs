ed0f3d431985d94fce34bf1143d29768d6058449
Сергей Лозовской <sergey_lozovskoy@outlook.com>
2019-12-16 23:58:22 +0300

    Done Snake

---
+++ Shake/Shake/FoodCreator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shake
+{
+    class FoodCreator
+    {
+        int mapWeidht;
+        int mapHeigth;
+        char sym;
+
+        Random random = new Random();
+
+        public FoodCreator(int mapWeidht, int mapHeigth, char sym)
+        {
+            this.mapHeigth = mapHeigth;
+            this.mapWeidht = mapWeidht;
+            this.sym = sym;
+        }
+        
+        public Point CreateFood()
+        {
+            int x = random.Next(2, mapWeidht - 2);
+            int y = random.Next(2, mapHeigth - 2);
+            return new Point(x, y, sym);
+        }
+    }
+}
--- Shake/Shake/Program.cs
+++ Shake/Shake/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Shake
@@ -10,6 +12,60 @@ namespace Shake
     {
         static void Main(string[] args)
         {
-        }
+           
+                Console.SetWindowSize(1, 1);
+                Console.SetBufferSize(80, 25);
+                Console.SetWindowSize(80, 25);
+          
+
+                Walls walls = new Walls(80, 25);
+                walls.Draw();
+
+                Point p1 = new Point(10, 10, '*');
+
+                Snake snake = new Snake(p1, 4, Direction.RIGHT);
+                snake.Draw();
+                snake.Move();
+
+                FoodCreator foodCreator = new FoodCreator(80, 25, '#');
+                Point food = foodCreator.CreateFood();
+                food.Draw();
+
+                while (true)
+                {
+                    if (walls.IsHit(snake) || snake.IsHitTail())
+                    {
+                        break;
+                    }
+
+                    if (snake.Eat(food))
+                    {
+                        food = foodCreator.CreateFood();
+                        food.Draw();
+                    }
+
+                    else
+                    {
+                        snake.Move();
+                    }
+
+                    Thread.Sleep(100);
+
+                    if (Console.KeyAvailable)
+                    {
+                        ConsoleKeyInfo key = Console.ReadKey();
+                        snake.HandleKey(key.Key);
+                    }
+
+                    snake.Move();
+                }
+
+
+                Console.Clear();
+                Console.SetCursorPosition(35, 10);
+                Console.WriteLine("Game Over!");
+                Console.ReadLine();
+          
+        } 
     }
 }
